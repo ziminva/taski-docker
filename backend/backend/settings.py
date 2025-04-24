2524d511bba3aa8f2917e7a0652d5ef453524a64
Vitaly <v.zimin@gt-sot.ru>
2025-04-24 08:58:54 +0300

    Flake8 checking

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -72,7 +72,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-
 
 
 # Password validation
